a402da6d9a06e3ce58bd89a077fc72ce396f8946
Bruno <bruno.ferraz@germinare.org.br>
2025-08-08 12:23:03 -0300

    fix(ai): deleting unused code

--- app/ai/embedding.py
+++ app/ai/embedding.py
@@ -34,15 +34,3 @@ def embedding_text(docs, question, k):
 def armazenar_vetores(id,question):
     print()  
 
-docs = []
-pasta = "app/ai/text/"
-for nome in os.listdir(pasta):
-    if nome.endswith(".txt"):
-        caminho = os.path.join(pasta, nome)
-        loader = TextLoader(caminho, encoding="utf-8")
-        docs.extend(loader.load())
-        documentos = docs
-        splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
-        docs_divididos = splitter.split_documents(documentos)
-        texto = embedding_text(docs_divididos,"O que é telemetria?",1)[0][0]
-        print(texto)
