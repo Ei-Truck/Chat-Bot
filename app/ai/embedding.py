aa7e9be998d3d60c3dc2cdcab1043304b53c8812
Bruno <bruno.ferraz@germinare.org.br>
2025-08-08 09:13:30 -0300

    fix(ai): updating embedding to use the .env parameter

--- app/ai/embedding.py
+++ app/ai/embedding.py
@@ -2,30 +2,29 @@ from sentence_transformers import SentenceTransformer
 from redis.commands.search.query import Query
 import numpy as np
 from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+# Resgatando o host do mongo
+load_dotenv()
+
+# Configura a API key
+host_mongo = os.getenv("MONGO_HOST")
+
 
 # Inicializando o modelo de embeddings
 model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
 # Configurando a conexão com o Mongo
-cliente = MongoClient("mongodb://localhost:27017/")
+client = MongoClient(host=host_mongo)
 
-def hist(content:str,user_id:str):
-    r.hset(f"histChat:{user_id}", mapping={
-        "content": content,
-        "genre": "hist",
-        "embedding": model.encode(content).astype(np.float32).tobytes(),
-    })
+# Embedding
+def embedding_text(content:str,user_id:str):
+    
 
     q = Query(
         "*=>[KNN 3 @embedding $vec AS vector_distance]"
     ).return_field("score").dialect(2)
 
-    query_text = 'Olá'
-    res = r.ft("vector_idx").search(
-        q, query_params={
-            "vec": model.encode(query_text).astype(np.float32).tobytes()
-        }
-    )
-    return res
-
-print(hist("Olá, tudo bem?", "user123"))
+    
+    
