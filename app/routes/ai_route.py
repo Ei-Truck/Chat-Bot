ca67ea605326fb1726c966fccda8bed25c91e5ef
Gueguelas <miguel0araujo0souza@gmail.com>
2025-08-25 22:10:16 -0300

    fix(schema/route): fixing user_id infos

--- app/routes/ai_route.py
+++ app/routes/ai_route.py
@@ -18,7 +18,7 @@ def chat():
     except ValidationError as err:
         return jsonify({"error": str(err)}), 400
 
-    answer:dict = question_for_gemini(validate_data["question"])
+    answer:dict = question_for_gemini(validate_data["question"], validate_data["user_id"])
 
     if not answer:
         return jsonify({"error": "Failed to generate answer"}), 500
--- app/schemas/question_schema.py
+++ app/schemas/question_schema.py
@@ -6,8 +6,8 @@ class AskSchema(Schema):
         "null": "field 'question' cannot be null.",
         "invalid": "field 'question' must be a string."
     })
-    user_id = fields.Str(required=True, error_messages={
+    user_id = fields.Int(required=True, error_messages={
         "required": "field 'user_id' is required.",
         "null": "field 'user_id' cannot be null.",
-        "invalid": "field 'user_id' must be a string."
+        "invalid": "field 'user_id' must be a integer."
     })
