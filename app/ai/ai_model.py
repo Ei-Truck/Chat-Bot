767d50868fe43f702b3c66e908cbafb1da604cc6
Bruno <bruno.ferraz@germinare.org.br>
2025-09-22 11:20:12 -0300

    fix(ai): changing judgment function

--- app/ai/ai_model.py
+++ app/ai/ai_model.py
@@ -45,42 +45,46 @@ def get_session_history(user_id, session_id) -> MongoDBChatMessageHistory:
 
 
 def gemini_resp(user_id, session_id, question):
+    session=f"{user_id}_{session_id}"
     llm = ChatGoogleGenerativeAI(
         model="gemini-2.5-flash", temperature=0.7, top_p=0.95, google_api_key=chave_api
     )
-    prompt_gemini = os.path.join(os.path.dirname(__file__), "prompt_gemini.txt")
+    
+    with open("./app/ai/text/prompt_juiz.txt", "r", encoding="utf-8") as f:
+        prompt_gemini_text = f.read()  
+
     system_prompt = (
         "system",
-        prompt_gemini
+        prompt_gemini_text
     )
-    example_prompt = ChatPromptTemplate.from_messages(
+    prompt = ChatPromptTemplate.from_messages(
         [
-            HumanMessagePromptTemplate.from_template("{human}"),
+            HumanMessagePromptTemplate.from_template("{input}"),
             AIMessagePromptTemplate.from_template("{ai}"),
         ]
     )
     shots = []
-    fewshots = FewShotChatMessagePromptTemplate(examples=shots, example_prompt=example_prompt)
+    fewshots = FewShotChatMessagePromptTemplate(examples=shots, example_prompt=prompt)
     prompt = ChatPromptTemplate.from_messages(
         [
             system_prompt,
             fewshots,
             MessagesPlaceholder("chat_history"),
-            ("human", "{usuario}"),
+            ("human", "{input}"),
         ]
     )
     base_chain = prompt | llm | StrOutputParser()
     chain = RunnableWithMessageHistory(
         base_chain,
         get_session_history=lambda _: get_session_history(user_id, session_id),
-        input_messages_key="usuario",
+        input_messages_key="input",
         history_messages_key="chat_history",
     )
     if question.lower() in ("sair", "end", "fim", "tchau", "bye"):
         return "Encerrando o chat."
     try:
         resposta = chain.invoke(
-            {"usuario": question}, config={"configurable": {"session_id": session_id}}
+            {"input": question}, config={"configurable": {"session_id": session}}
         )
         return resposta
     except Exception as e:
@@ -88,46 +92,49 @@ def gemini_resp(user_id, session_id, question):
 
 
 # Verificar Resposta
-def juiz_resposta(pergunta: str, resposta: str) -> str:
+def juiz_resposta(question: str, answer: str,user_id: int,session_id: int) -> str:
+    session=f"{user_id}_{session_id}"
+    user = "Pergunta: "+question+"\nResposta: "+answer
     juiz = ChatGoogleGenerativeAI(
         model="gemini-2.5-flash", temperature=0.5, google_api_key=chave_api
     )
-
-    prompt_juiz = os.path.join(os.path.dirname(__file__), "prompt_juiz.txt")
+    with open("./app/ai/text/prompt_juiz.txt", "r", encoding="utf-8") as f:
+        prompt_juiz_text = f.read()
     system_prompt = (
         "system",
-        prompt_juiz
+        prompt_juiz_text
     )
     prompt = ChatPromptTemplate.from_messages(
         [
-            HumanMessagePromptTemplate.from_template("{human}"),
+            HumanMessagePromptTemplate.from_template("{input}"),
             AIMessagePromptTemplate.from_template("{ai}"),
         ]
     )
+    shots = []
+    fewshots = FewShotChatMessagePromptTemplate(examples=shots, example_prompt=prompt)
     prompt = ChatPromptTemplate.from_messages(
         [
             system_prompt,
+            fewshots,
             MessagesPlaceholder("chat_history"),
-            ("human", "{usuario}"),
+            ("human", "{input}"),
         ]
     )
     base_chain = prompt | juiz | StrOutputParser()
     chain = RunnableWithMessageHistory(
         base_chain,
-        input_messages_key="usuario",
+        get_session_history=lambda _: get_session_history(user_id, session_id),
+        input_messages_key="input",
         history_messages_key="chat_history",
     )
     try:
         resposta_juiz = chain.invoke(
-            {
-                "usuario": f"Pergunta: {pergunta}\nResposta do Chatbot: {resposta}\nAvalie a resposta do chatbot com base na pergunta e responda em formato JSON conforme o modelo fornecido no prompt."
-            }
+            {"input": user}, config={"configurable": {"session_id": session}}
         )
-    except Exception as e:
-        return f"Não foi possível avaliar a resposta: {e}"
+        resposta_juiz = resposta_juiz.strip()
+        if resposta_juiz.startswith("```json"):
+            resposta_juiz = resposta_juiz[len("```json"):].rstrip("```").strip()
+        return resposta_juiz
     
-    resposta_juiz = resposta_juiz.content.strip()
-    if resposta_juiz.startswith("```json"):
-        resposta_juiz = resposta_juiz[len("```json"):].rstrip("```").strip()
-
-    return resposta_juiz
+    except Exception as e:
+        return f"Não foi possível responder: {e}"
