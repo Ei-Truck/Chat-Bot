8432a48f799499c6b59024dea9958c97941f347b
Bruno28-02 <bruno.ferraz@germinare.org.br>
2025-10-15 11:18:02 -0300

    refactor(ai): updating requirements v1.2

--- app/ai/ai_rag.py
+++ app/ai/ai_rag.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
-from google.generativeai import types
-import google.generativeai as genai
+from google import genai
+from google.genai import types
 from sklearn.metrics.pairwise import cosine_similarity
 from dotenv import load_dotenv
 import pymongo
