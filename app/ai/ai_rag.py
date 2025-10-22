e590e69c97c4b9ec77ac34676a9afa724e9c6fef
Bruno28-02 <bruno.ferraz@germinare.org.br>
2025-10-22 15:15:35 -0300

    fix(ai): removing unused imports

--- app/ai/ai_rag.py
+++ app/ai/ai_rag.py
@@ -1,8 +1,6 @@
 import os
-import numpy as np
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import SentenceTransformerEmbeddings
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.document_loaders import TextLoader
 from dotenv import load_dotenv
